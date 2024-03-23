c479b7705761a504df793dcb4d77fe00a621b7ed
FireElia112 <e.schmid.wgt@gmail.com>
2024-03-23 16:06:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
-// Speicherort für die hochgeladenen Fotos
+// Speicherort für hochgeladenen Fotos
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/') // Das Verzeichnis 'uploads/' muss existieren
@@ -17,33 +17,27 @@ const storage = multer.diskStorage({
   }
 });
 
-// Konfiguration von Multer
 const upload = multer({ storage: storage });
 
-// POST-Endpunkt für das Hochladen von Fotos
+//Hochladen von Fotos
 app.post('/uploads', upload.single('photo'), (req, res) => {
   if (!req.file) {
     return res.status(400).send('Es wurde keine Datei hochgeladen.');
   }
 
-  // Hier kannst du weitere Logik hinzufügen, z.B. Datenbankoperationen usw.
-
   res.send('Foto erfolgreich hochgeladen: ' + req.file.filename);
 });
 
-// GET-Endpunkt, um alle hochgeladenen Dateien zurückzugeben
-// GET-Endpunkt, um alle hochgeladenen Dateien zurückzugeben
-// GET-Endpunkt, um alle hochgeladenen Bilder zurückzugeben
+
 app.get('/uploads', (req, res) => {
   fs.readdir('uploads/', (err, files) => {
     if (err) {
       return res.status(500).send('Ein Fehler ist aufgetreten.');
     }
 
-    // Array, um Bilddaten zu speichern
+    //Bilddaten speichern
     let imageData = [];
 
-    // Schleife durch jede Datei im Verzeichnis
     files.forEach((file) => {
       const filePath = `uploads/${file}`;
 
@@ -54,13 +48,13 @@ app.get('/uploads', (req, res) => {
           return;
         }
 
-        // Hinzufügen der Bilddaten zum imageData-Array
+        // Hinzufügen der Bilddaten zum Array
         imageData.push({
           filename: file,
           data: data.toString('base64') // Bilddaten als Base64 kodiert
         });
 
-        // Wenn alle Bilder gelesen wurden, sende die Antwort
+        // Wenn alles gelesen, sende Antwort
         if (imageData.length === files.length) {
           res.json(imageData);
         }
